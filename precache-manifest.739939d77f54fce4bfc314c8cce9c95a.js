7fa8e44f74bec8caf762019e723e0e07294b4e35
himi <himanijain668@gmail.com>
2020-07-26 14:08:52 +0530

    Updates

---
+++ precache-manifest.739939d77f54fce4bfc314c8cce9c95a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fc2f17024d05172c14323cc3d9518b",
+    "url": "/himanijain11/Login_System_Assignment/index.html"
+  },
+  {
+    "revision": "fee35731dc310333756e",
+    "url": "/himanijain11/Login_System_Assignment/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d25d15611dfece25fd56",
+    "url": "/himanijain11/Login_System_Assignment/static/js/2.09560aa8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/himanijain11/Login_System_Assignment/static/js/2.09560aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee35731dc310333756e",
+    "url": "/himanijain11/Login_System_Assignment/static/js/main.f13b66d0.chunk.js"
+  },
+  {
+    "revision": "7b8c8f49ad33179d9cf4",
+    "url": "/himanijain11/Login_System_Assignment/static/js/runtime-main.9069ef20.js"
+  }
+]);
